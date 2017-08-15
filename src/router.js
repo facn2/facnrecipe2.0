afa77a69c1c79a7f661b2ea0af9c9e78776e9a8a
heathercoraje <yhk7830@hotmail.com>
2017-08-15 19:40:17 +0300

    add hash functions and routes for login and signup relates #26

--- src/router.js
+++ src/router.js
@@ -1,19 +1,31 @@
 const http = require('http');
 const pg = require('pg');
-const {handleHomeRoute, handleCuisine, handleNewRecipe, handlePublic, handle404} = require('./handlers');
+const {
+  handleHomeRoute,
+  handleCuisine,
+  handleNewRecipe,
+  handlePublic,
+  handleLogin,
+  handleSignup,
+  handle404
+} = require('./handlers');
 
 const router = (request, response) => {
 
   const endpoint = request.url.split('/')[1];
 
   if (endpoint === '') {
-		handleHomeRoute(response);
+    handleHomeRoute(response);
   } else if (endpoint.match("^Asian|Arabic|British|Italian$")) {
     handleCuisine(request, response);
   } else if (endpoint === 'add') {
     handleNewRecipe(request, response)
   } else if (endpoint.match(/public/)) {
     handlePublic(request, response);
+  } else if (endpoint.match(/login/)) {
+    handleLogin(request, response);
+  } else if (endpoint.match(/signup/)) {
+    handleSignup(request, response);
   } else {
     handle404(request, response);
   }
