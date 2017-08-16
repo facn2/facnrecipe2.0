92834cfe6418925eb92be6b9d836cdb8653aef9d
heathercoraje <yhk7830@hotmail.com>
2017-08-16 09:26:05 +0300

    fix queries according to table/columns in addData.js and getData.js relates #29

--- src/queries/addData.js
+++ src/queries/addData.js
@@ -1,10 +1,15 @@
-const dbConnection = require ('../database/db_connection');
+const dbConnection = require('../database/db_connection');
 
-const addNewRecipeQuery = `INSERT INTO recipe (recipe_name, recipe_ingredients, recipe_directions, recipe_origin) VALUES ($1, $2, $3, $4);`;
+const addNewRecipeQuery = `INSERT INTO recipe (name, ingredients, procedure, cuisine) VALUES ($1, $2, $3, $4);`;
 
 const addNewRecipe = (recipeInput, callback) => {
-  const { name, ingredients, directions, origin} = recipeInput
-    dbConnection.query(addNewRecipeQuery, [name, ingredients, directions, origin], (err, res) => {
+  const {
+    name,
+    ingredients,
+    directions,
+    origin
+  } = recipeInput
+  dbConnection.query(addNewRecipeQuery, [name, ingredients, directions, origin], (err, res) => {
     if (err) {
       console.log(err)
       return callback(err);
